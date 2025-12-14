275b491e80792425433432ee2fc12983ebbb32c6
Sunil Bastola <sunilbytes56@gmail.com>
2025-12-14 12:57:24 +1100

    Add the application models

--- backend/jobs/admin.py
+++ backend/jobs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Job, Applicant
 
-# Register your models here.
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'location', 'salary', 'created_at')
+    search_fields = ('title', 'description', 'company__username')
+    list_filter = ('location', 'created_at')
+
+@admin.register(Applicant)
+class ApplicantAdmin(admin.ModelAdmin):
+    list_display = ('user', 'job', 'applied_at')
+    search_fields = ('user__username', 'job__title')
+    list_filter = ('applied_at',)
